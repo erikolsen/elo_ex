c106b9a15bacfdecef804f3f743acdd96255f961
Erik Olsen <erik.s.olsen@gmail.com>
2017-01-05 23:26:38 -0600

    Calculate change in rating given a winner_rating and loser_rating

--- lib/elo_calculator.ex
+++ lib/elo_calculator.ex
@@ -1,10 +1,9 @@
 defmodule EloEx.EloCalculator do
+  import Math
+
   def rating_change(winner_rating, loser_rating) do
-    cond do
-      winner_rating < loser_rating ->
-        5
-      winner_rating > loser_rating ->
-        10
-    end
+    (50.00 * ( 1.00 - (1.00 / (pow(10.00, Float.round(((loser_rating - winner_rating) / 400.00), 2)) + 1.00))))
+    |> Float.ceil
+    |> trunc
   end
 end
--- test/elo_calculator_test.exs
+++ test/elo_calculator_test.exs
@@ -1,11 +1,36 @@
 defmodule EloEx.EloCalculatorTest do
   use ExUnit.Case, async: true
   alias EloEx.EloCalculator
-test "winner_rating is greater" do
-    assert EloCalculator.rating_change(200, 100) == 10
+
+  test "ratings are even" do
+    assert EloCalculator.rating_change(1000, 1000) == 25
   end
 
-  test "winner_rating is lower" do
-    assert EloCalculator.rating_change(100, 200) == 5
+  ### GREATER
+
+  test "winner_rating is greater by double" do
+    assert EloCalculator.rating_change(2000, 1000) == 1
+  end
+
+  test "winner_rating is greater by 100" do
+    assert EloCalculator.rating_change(1100, 1000) == 18
+  end
+
+  test "winner_rating is greater by 200" do
+    assert EloCalculator.rating_change(1200, 1000) == 13
+  end
+
+  ### LOWER
+
+  test "winner_rating is lower by 100" do
+    assert EloCalculator.rating_change(1000, 1100) == 33
+  end
+
+  test "winner_rating is lower by 200" do
+    assert EloCalculator.rating_change(1000, 1200) == 38
+  end
+
+  test "winner_rating is lower by double" do
+    assert EloCalculator.rating_change(1000, 2000) == 50
   end
 end
